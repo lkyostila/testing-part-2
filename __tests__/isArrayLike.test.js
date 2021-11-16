5aa686695c20f2dfd1454fe62b10143f8646a5f4
lkyostila <kyostila26@gmail.com>
2021-11-16 17:30:47 +0200

    Fixing

--- __tests__/isArrayLike.test.js
+++ __tests__/isArrayLike.test.js
@@ -1,19 +1,27 @@
 import isArrayLike from "../library/src/isArrayLike";
 
+const person = {
+  firstname: "John",
+  lastname: "Doe"
+};
+
 describe("Unit tests for isArrayLike.js", () => {
-  test("Input a correct array.", () => {
+  test("Input a correct array which should return true.", () => {
     expect(isArrayLike([1, 2, 3])).toBe(true);
   });
-  test("Input a correct array.", () => {
-    expect(isArrayLike(document.body.childNodes)).toBe(true);
+  test("Input a property of an object which should return true.", () => {
+    expect(isArrayLike(person.firstname)).toBe(true);
   });
-  test("Input a correct array.", () => {
+  test("Input a property of an object which should return true.", () => {
+    expect(isArrayLike(person.lastname)).toBe(true);
+  });
+  test("Input a string which should return true.", () => {
     expect(isArrayLike("testing")).toBe(true);
   });
-  test("Input an incorrect array.", () => {
+  test("Input 'null' which should return false.", () => {
     expect(isArrayLike(null)).toBe(false);
   });
-  test("Input an incorrect array.", () => {
-    expect(isArrayLike(toString())).toBe(false);
+  test("Input a function which should return false.", () => {
+    expect(isArrayLike(person.lastname.length)).toBe(false);
   });
 });
